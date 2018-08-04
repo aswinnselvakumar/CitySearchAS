c3d2db080367b020dfbbb2cdf2feec77101fe09c
Selvakumar <asselvakumar@deloitte.com>
2018-08-04 19:43:13 +0530

    Moved the sub project outside*

--- CitySearch-Parent/CitySearch-core/src/main/java/com/app/dao/CityDaoInterface.java
+++
@@ -1,7 +0,0 @@
-package com.app.dao;
-
-import com.app.entities.CityInfo;
-
-public interface CityDaoInterface {
-  public void addCity(CityInfo city);
-}
--- CitySearch-Parent/CitySearch-core/src/main/java/com/app/utils/CitySearchConstants.java
+++
@@ -1,7 +0,0 @@
-package com.app.utils;
-
-public class CitySearchConstants {
-  public static final String MANDATORY_PARAM_EMPTY="The parameter '%s' is mandatory";
-  public static final String ONE_MANDATORY_PARAM_REQUIRED="At least one mandatroy parameter is reqired";
-  public static final String ENTITY_ALREADY_PRESENT="Entity already exist with the current details";
-}
