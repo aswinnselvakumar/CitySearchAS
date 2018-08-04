8399121c195294684f9a86b558b1823d93b0d56c
Selvakumar <asselvakumar@deloitte.com>
2018-08-04 16:48:38 +0530

    Inital Commit

---
+++ CitySearch-Parent/src/main/java/CitySearch_Parent/CitySearch_Parent/App.java
@@ -0,0 +1,13 @@
+package CitySearch_Parent.CitySearch_Parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
